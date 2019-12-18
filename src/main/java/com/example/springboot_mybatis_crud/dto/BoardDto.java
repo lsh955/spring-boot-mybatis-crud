09c522664d34b39d68b8a971f853ce270f9f0b5e
이승환 <knetdev@knetbiz.com>
2019-12-19 08:42:18 +0900

    dto class 작성

--- src/main/java/com/example/springboot_mybatis_crud/dto/BoardDto.java
+++ src/main/java/com/example/springboot_mybatis_crud/dto/BoardDto.java
@@ -6,6 +6,58 @@ package com.example.springboot_mybatis_crud.dto;
  */
 public class BoardDto {
 
-
-
+    private Integer id;
+    private String name;
+    private String tel;
+    private String age;
+    private String title;
+    private String content;
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getTel() {
+        return tel;
+    }
+    
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    
+    public String getAge() {
+        return age;
+    }
+    
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getContent() {
+        return content;
+    }
+    
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
