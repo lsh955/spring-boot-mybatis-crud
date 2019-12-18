8d7c1bea67e83663dfe1e0b17aac5cf554ac40c0
이승환 <knetdev@knetbiz.com>
2019-12-19 08:42:57 +0900

    인터페이스에 @Mapper 어노테이션 입력

--- src/main/java/com/example/springboot_mybatis_crud/dao/UpdateDao.java
+++ src/main/java/com/example/springboot_mybatis_crud/dao/UpdateDao.java
@@ -1,9 +1,12 @@
 package com.example.springboot_mybatis_crud.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * @author 이승환
  * @since 2019-12-18
  */
+@Mapper
 public interface UpdateDao {
 
 
