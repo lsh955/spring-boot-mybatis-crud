a5efb9f6eb93a8b1dbf939c08cb5b52fc87b6569
이승환 <knetdev@knetbiz.com>
2019-12-19 08:43:27 +0900

    서비스단은 @Service 작성

--- src/main/java/com/example/springboot_mybatis_crud/service/BoardService.java
+++ src/main/java/com/example/springboot_mybatis_crud/service/BoardService.java
@@ -1,9 +1,12 @@
 package com.example.springboot_mybatis_crud.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author 이승환
  * @since 2019-12-18
  */
+@Service
 public class BoardService {
 
 
