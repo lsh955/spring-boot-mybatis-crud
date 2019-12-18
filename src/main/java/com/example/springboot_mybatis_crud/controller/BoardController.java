56f012ba7277aa18f27817688679a8916ee41951
이승환 <knetdev@knetbiz.com>
2019-12-19 08:38:35 +0900

    리팩토링

--- src/main/java/com/example/springboot_mybatis_crud/controller/BoardController.java
+++ src/main/java/com/example/springboot_mybatis_crud/controller/BoardController.java
@@ -1,11 +1,18 @@
 package com.example.springboot_mybatis_crud.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author 이승환
  * @since 2019-12-18
  */
+@Controller
 public class BoardController {
 
-
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 
 }
